fa12f8b238ca2e01a4bbf13f444fbea791fc9a93
sidahmedabdelillah <s_bentegar@enst.dz>
2024-03-31 14:18:14 +0200

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ fn main() {
 fn handle_client(mut stream: TcpStream) {
     let mut buf = [0; 512];
     let pong = "+PONG\r\n";
-    print!("handling client ");
+    println!("handling client ");
     loop {
         let bytes_read = stream.read(&mut buf).expect("Failed to read from client");
         if bytes_read == 0 {
