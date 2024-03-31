175f5749e27e98b953e7710d38c4afcb455e4fdc
sidahmedabdelillah <s_bentegar@enst.dz>
2024-03-31 14:29:08 +0200

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,47 @@
-use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
+use anyhow::Error;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
-fn main() {
+#[tokio::main]
+async fn main()-> Result<(),Error> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
+    let addr =  "127.0.0.1:8080".to_string();
+
     // Uncomment this block to pass the first stage
     //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => {
-                handle_client(_stream);
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+    let listener = TcpListener::bind(&addr).await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        
     }
 }
 
 
 fn handle_client(mut stream: TcpStream) {
-    let mut buf = [0; 512];
-    let pong = "+PONG\r\n";
     println!("handling client ");
-    loop {
-        let bytes_read = stream.read(&mut buf).expect("Failed to read from client");
-        if bytes_read == 0 {
-            return;
+
+    tokio::spawn(async move {
+        let mut buf = [0; 512];
+        let pong = "+PONG\r\n";
+
+        // In a loop, read data from the socket and write the data back.
+        loop {
+            let n = stream
+                .read(&mut buf)
+                .await
+                .expect("failed to read data from socket");
+
+            if n == 0 {
+                return;
+            }
+
+            stream
+                .write_all(pong.as_bytes())
+                .await
+                .expect("failed to write data to socket");
         }
-        stream.write_all(pong.as_bytes()).unwrap();
-    }
+    });
 }
