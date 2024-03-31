055ffd8c8ade554dfe22f1cab3806d30030fb460
sidahmedabdelillah <s_bentegar@enst.dz>
2024-03-31 16:13:44 +0200

    pass 1st stage

---
+++ src/decoder.rs
@@ -0,0 +1,113 @@
+use anyhow::Error;
+
+#[derive(Debug)]
+pub enum PacketTypes {
+    SimpleString(String),
+    INTEGER(usize),
+    BulkString(String),
+    Array(Vec<PacketTypes>),
+}
+
+pub enum ComandTypes {
+    PING,
+    ECHO,
+}
+
+impl PacketTypes {
+    pub fn parse(buffer: &[u8]) -> Result<(Self, usize), Error> {
+        match buffer[0] as char {
+            '+' => {
+                return parse_simple_string(buffer);
+            }
+            '$' => {
+                return parse_bulk_string(buffer);
+            }
+            '*' => {
+                return parse_array(buffer);
+            }
+            _ => {
+                println!("Invalid command {}", buffer[0] as char);
+                return Err(anyhow::anyhow!("Invalid command {}", buffer[0]));
+            }
+        }
+    }
+
+    pub fn to_string(&self) -> String {
+        match self {
+            PacketTypes::SimpleString(s) => format!("+{}\r\n", s),
+            PacketTypes::BulkString(s) => format!("${}\r\n{}\r\n", s.len(), s),
+            PacketTypes::Array(a) => {
+                let mut result = String::from("*");
+                result.push_str(&a.len().to_string());
+                result.push_str("\r\n");
+                for packet in a {
+                    result.push_str(&packet.to_string());
+                }
+                result
+            },
+            _ => {
+                println!("Invalid command");
+                return String::from("");
+            },
+        }
+    }
+}
+
+fn parse_simple_string(buffer: &[u8]) -> Result<(PacketTypes, usize), Error> {
+    if let Some((simple_string, pos)) = read_until_crlf(&buffer[1..]) {
+        let simple_string = std::str::from_utf8(simple_string)?.to_string();
+        return Ok((PacketTypes::SimpleString(simple_string), pos));
+    } else {
+        return Err(anyhow::anyhow!("Can't parse simple string"));
+    }
+}
+
+fn parse_bulk_string(buffer: &[u8]) -> Result<(PacketTypes, usize), Error> {
+    let (bulk_str_len, pos) = if let Some((bulk_str_len, pos)) = read_until_crlf(&buffer[1..]) {
+        let bulk_str_len = std::str::from_utf8(bulk_str_len)?.parse::<usize>()?;
+        (bulk_str_len, pos)
+    } else {
+        return Err(anyhow::anyhow!("Invalid command"));
+    };
+
+    let end_of_bulk_str = pos + bulk_str_len;
+
+    let string = std::str::from_utf8(&buffer[pos..end_of_bulk_str]).unwrap();
+
+    let total_parsed = end_of_bulk_str + 2;
+    return Ok((PacketTypes::BulkString(string.to_string()), total_parsed));
+}
+
+fn parse_array(buffer: &[u8]) -> Result<(PacketTypes, usize), Error> {
+    let (array_len, mut pos) = if let Some((array_len, pos)) = read_until_crlf(&buffer[1..]) {
+        let array_len_str = std::str::from_utf8(array_len)?;
+        let array_len = array_len_str.parse::<usize>()?;
+        (array_len, pos)
+    } else {
+        println!("can't read to crlf");
+        return Err(anyhow::anyhow!("Invalid command"));
+    };
+
+    let mut array = Vec::new();
+    for _ in 0..array_len {
+        let (packet, len) = PacketTypes::parse(&buffer[pos..])?;
+        array.push(packet);
+        pos += len;
+    }
+
+    return Ok((PacketTypes::Array(array), pos));
+}
+
+pub fn parse_message(buffer: &[u8]) -> Result<(PacketTypes, usize), anyhow::Error> {
+    return PacketTypes::parse(buffer);
+}
+
+fn read_until_crlf(buffer: &[u8]) -> Option<(&[u8], usize)> {
+    for i in 1..buffer.len() {
+        if buffer[i - 1] == b'\r' && buffer[i] == b'\n' {
+            return Some((&buffer[0..(i - 1)], i + 2 as usize));
+        }
+    }
+
+    return None;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
-use anyhow::Error;
+use std::f32::consts::E;
+use std::process::Command;
+use std::ptr::null;
+
+use anyhow::{Context, Error};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 
+mod decoder;
+
+use decoder::PacketTypes;
+
 #[tokio::main]
-async fn main()-> Result<(),Error> {
+async fn main() -> Result<(), Error> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    let addr =  "127.0.0.1:6379".to_string();
+    let addr = "127.0.0.1:6379".to_string();
 
     // Uncomment this block to pass the first stage
     //
@@ -19,13 +27,11 @@ async fn main()-> Result<(),Error> {
     }
 }
 
-
 fn handle_client(mut stream: TcpStream) {
     println!("handling client ");
 
     tokio::spawn(async move {
         let mut buf = [0; 512];
-        let pong = "+PONG\r\n";
 
         // In a loop, read data from the socket and write the data back.
         loop {
@@ -38,10 +44,51 @@ fn handle_client(mut stream: TcpStream) {
                 return;
             }
 
-            stream
-                .write_all(pong.as_bytes())
-                .await
-                .expect("failed to write data to socket");
+            let (packet, _) = decoder::parse_message(&buf[..n]).unwrap();
+
+            match (packet) {
+                PacketTypes::Array(packets) => {
+                    let packet1 = packets.get(0);
+                    let packet2 = packets.get(1);
+                    match (packet1, packet2) {
+                        (
+                            Some(PacketTypes::BulkString(bulk1)),
+                            Some(PacketTypes::BulkString(bulk2)),
+                        ) => {
+                            let command = bulk1.as_str();
+                            match command.to_uppercase().as_str() {
+                                "ECHO" => {
+                                    let res = PacketTypes::BulkString(bulk2.to_string());
+                                    let echo = res.to_string();
+                                    stream.write_all(echo.as_bytes()).await.unwrap();
+                                }
+                                _ => {
+                                    println!("unsupported command {}", command);
+                                }
+                            }
+                        }
+                        (Some(PacketTypes::BulkString(bulk1)), None) => {
+                            let command = bulk1.as_str();
+                            match command.to_uppercase().as_str() {
+                                "PING" => {
+                                  let res = PacketTypes::SimpleString("PONG".to_string());
+                                  let pong = res.to_string();
+                                  stream.write_all(pong.as_bytes()).await.unwrap();
+                                }
+                                _ => {
+                                    println!("unsupported command {}", command);
+                                }
+                            }
+                        }
+                        _ => {
+                            anyhow::anyhow!("Invalid command");
+                        }
+                    }
+                }
+                _ => {
+                    anyhow::anyhow!("Commands must be of type array");
+                }
+            }
         }
     });
 }
