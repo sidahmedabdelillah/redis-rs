247c1ec81e5d30013a0ab5ec6ab7576ba0c8f14e
sidahmedabdelillah <s_bentegar@enst.dz>
2024-03-31 19:30:40 +0200

    after cargo fix

--- src/client.rs
+++ src/client.rs
@@ -1,13 +1,30 @@
-use std::{vec};
+use std::vec;
 
 use anyhow::Error;
 
 use tokio::{
-    io::{AsyncWriteExt},
+    io::{AsyncReadExt, AsyncWriteExt},
     net::TcpStream,
 };
 
 use crate::{decoder::PacketTypes, server::Server};
+
+struct Client<'a> {
+    server: &'a Server,
+    stream: &'a mut TcpStream,
+}
+
+impl<'a> Client<'a> {
+    pub async fn send_commands(&mut self, commands: Vec<String>) {
+        let commands: Vec<PacketTypes> = commands
+            .iter()
+            .map(|c| PacketTypes::BulkString(c.to_string()))
+            .collect();
+        let commands = PacketTypes::Array(commands);
+        let str = commands.to_string();
+        self.stream.write_all(str.as_bytes()).await.unwrap();
+    }
+}
 
 pub async fn init_client(server: &Server) -> Result<(), Error> {
     let host = &server.replicat_of.as_ref().unwrap().0;
@@ -17,28 +34,26 @@ pub async fn init_client(server: &Server) -> Result<(), Error> {
 
     let mut stream = TcpStream::connect(&addr).await.unwrap();
 
+    let mut client = Client {
+        server: &server,
+        stream: &mut stream,
+    };
+
+    let mut handshake_buf: [u8; 128] = [0; 128];
     println!("Debug: Connected to master at {}", &addr);
-    
-    let ping = PacketTypes::BulkString("ping".to_string());
-    let commands: Vec<PacketTypes> = vec![ping];
-    let commands = PacketTypes::Array(commands);
-    let str = commands.to_string();
 
-    println!("Debug: Sending {}", str);
-    stream.write_all(str.as_bytes()).await.unwrap();
+    println!("Debug: Sending ping");
+    client.send_commands(vec!["ping".to_string()]).await;
 
-    stream.shutdown().await.unwrap();
+    let _ = client.stream.read(&mut handshake_buf);
 
-    let mut stream = TcpStream::connect(&addr).await.unwrap();
-    let replconf = PacketTypes::BulkString("REPLCONF".to_string());
-    let listening_port = PacketTypes::BulkString("listening-port".to_string());
-    let port = PacketTypes::BulkString(server.port.clone());
-
-    let commands: Vec<PacketTypes> = vec![replconf, listening_port, port];
-    let commands = PacketTypes::Array(commands);
-    let str = commands.to_string();
-    stream.write_all(str.as_bytes()).await.unwrap();
-
+    client
+        .send_commands(vec![
+            "replconf".to_string(),
+            "listening-port".to_string(),
+            server.port.clone(),
+        ])
+        .await;
 
     return Ok(());
 }
